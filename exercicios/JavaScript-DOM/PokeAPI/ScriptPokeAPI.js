786b0e0fa3b8d5125a444c4cfa0b6da5f2e8ac62
-- <-->
2024-11-29 19:07:22 -0300

    style

--- exercicios/JavaScript-DOM/PokeAPI/ScriptPokeAPI.js
+++ exercicios/JavaScript-DOM/PokeAPI/ScriptPokeAPI.js
@@ -9,9 +9,32 @@ document.getElementById('searchButton').addEventListener('click', () => {
             document.getElementById('pokemonImage').src = data.sprites.front_default;
             document.getElementById('pokemonType').textContent = `Tipo(s): ${data.types.map(type => type.type.name).join(', ')}`;
 
+            data.abilities.forEach(ability => {
+                const abilityLi = document.createElement('li');
+                habilidade = ability.ability.name;
+                abilityLi.textContent = habilidade;
+                
+                fetch(`https://pokeapi.co/api/v2/ability/${habilidade}`)
+                    .then(response => response.json())
+                    .then(abilityData =>
+                        abilityLi.appendChild(document.createTextNode(` - ${abilityData.effect_entries[0].effect}`)));
+                // Adicionando a lista de habilidades ao DOM
+                document.getElementById('pokemonAbilities').appendChild(abilityLi);
+            });
+
             // Exibindo as estatísticas
-            let stats = data.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ');
-            document.getElementById('pokemonStats').textContent = `Estatísticas: ${stats}`;
+            //let stats = data.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ');
+
+            const statUlist = document.getElementById('pokemonStats');
+
+            data.stats.forEach(stat => {
+                const statLi = document.createElement('li');
+                statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`
+                console.log(`${stat.stat.name}: ${stat.base_stat}`)
+                statUlist.appendChild(statLi);
+            });
+
+            //document.getElementById('pokemonStats').textContent = `Estatísticas: ${stats}`;
         })
         .catch(error => {
             alert('Pokémon não encontrado!');
